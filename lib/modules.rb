e2efc5fc99f183ef2000cf41683aef2728488418
Daniel Bond <gitbits@danielbond.org>
2007-06-29 00:05:37 +0200

    * added basic nickobfuscator that works  * added die command (wow!)

--- lib/modules.rb
+++ lib/modules.rb
@@ -1,26 +1,11 @@
-module IRC
+module IRCHelpers
 	class NickObfusicator
 		def NickObfusicator.run( old_nick )
-			# find stuff to replace
-			new_nick = old_nick
-			nick_map = { "a" => "4", "l" => "1", "o" => "0", "e" => "3" } 
-
-			candidates = old_nick.scan(/([aloe])/)
-			other_options = ["-","_"]
-
-			i_replacements = 0
-
-			if candidates.size > 0
-				candidates = candidates.uniq 
-				candidates.each {|c| new_nick = new_nick.to_s.sub("a", nick_map["a"]); i_replacements += 1 }
-			end
-		
-			if i_replacements == 0
-				new_nick += other_options[rand((other_options.size) -1)]
-				i_replacements += 1
-			end
-			new_nick
+			# replace old_nick
+			old_nick[rand(old_nick.size - 1)] = (rand(122-97) + 97).chr
+			return old_nick
 		end
 	end
 end
 
+IRCHelpers::NickObfusicator.run("Mr_Bond")
