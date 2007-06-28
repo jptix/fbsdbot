82baa6ae8feeee19703afd24adebce922946b2fd
Daniel Bond <daniel@a7j.danielbond.org>
2007-06-28 04:37:45 +0200

    Initial commit

---
+++ lib/boot.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'IRC'
+require 'yaml'
+
+# check for config file
+config_file = File.dirname(__FILE__) + '/../bin/towlie.conf'
+unless File.exists?( config_file )
+	puts "Please create a config-file (YAML syntax) and put it in #{config_file}"
+	exit 1
+end
+
+@config = YAML.load( File.open(config_file) )
