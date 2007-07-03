97067562afd77563c49f5ffa402f33422c6b7bf4
Daniel Bond <gitbits@danielbond.org>
2007-07-04 00:16:40 +0200

    renamed bot to fbsdbot and commited

--- lib/boot.rb
+++ lib/boot.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require File.dirname(__FILE__) + '/modules.rb'
 
 # check for config file
-config_file = File.dirname(__FILE__) + '/../bin/towlie.conf'
+config_file = File.dirname(__FILE__) + '/../bin/bot.conf'
 unless File.exists?( config_file )
 	puts "Please create a config-file (YAML syntax) and put it in #{config_file}"
 	exit 1
