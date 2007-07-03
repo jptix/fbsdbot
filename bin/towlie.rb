ab02108e331d30859b8c18bc090cbbb023d954a5
Daniel Bond <gitbits@danielbond.org>
2007-07-03 23:48:45 +0200

    Revert " * added basic nickobfuscator that works"
    This reverts commit e2efc5fc99f183ef2000cf41683aef2728488418.

--- bin/towlie.rb
+++ bin/towlie.rb
@@ -2,12 +2,13 @@
 
 require File.dirname(__FILE__) + '/../lib/boot.rb'
 
+	puts @config.inspect
+
 	bot = IRC.new(@config['nick'], @config['host'], @config['port'], "can you say marclar?")
 	IRCEvent.add_callback('nicknameinuse') {|event|
-		bot.ch_nick( IRCHelpers::NickObfusicator.run(bot.nick) )
+		bot.ch_nick = IRC::NickObfusicator.run(bot.nick)
 	}
-	#IRCEvent.add_callback('endofmotd') { |event| bot.add_channel('#bot-test.no') }
-	IRCEvent.add_callback('endofmotd') { |event| bot.add_channel('#FreeBSD.no') }
+	IRCEvent.add_callback('endofmotd') { |event| bot.add_channel('#bot-test.no') }
   #IRCEvent.add_callback('join') {|event| bot.send_message( event.channel, "Hello #{event.from}" ) }
 	IRCEvent.add_callback('privmsg') do |event| 
 
@@ -15,12 +16,12 @@ require File.dirname(__FILE__) + '/../lib/boot.rb'
 		 unless event.channel == bot.nick
 		 	#bot.send_message( event.from, "event: #{event.inspect}") 
 		 	#bot.send_message( event.from, "bot: #{bot.inspect}")
-		 	if event.message == "!die"
-				bot.send_quit()
-			end
-		 else
+			else
 				bot.send_message( event.from, "Why are you talking to me? Go bother someone else please .." )
      end 
 	end
 
   bot.connect
+
+# #<IRCEvent:0xb799aedc @hostmask="danb@noc.nsn.no", @from="Mr_Bond", @message="h", @channel="#bot-test.no", @event_type="privmsg", @user=#<IRCUser:0xb799ad4c @username="Mr_Bond">, @stats=["Mr_Bond", "danb@noc.nsn.no", "PRIVMSG", "#bot-test.no"]>
+#<IRCEvent:0xb799a98c @hostmask="danb@noc.nsn.no", @from="Mr_Bond", @message="bitch", @channel="Towli3", @event_type="privmsg", @user=#<IRCUser:0xb799ad4c @username="Mr_Bond">, @stats=["Mr_Bond", "danb@noc.nsn.no", "PRIVMSG", "Towli3"]>
