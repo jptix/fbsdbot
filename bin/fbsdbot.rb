ef832d33577065589d5e99ad43b1e87f31049a40
Daniel Bond <gitbits@danielbond.org>
2007-07-04 05:29:15 +0200

    push all privmsgs into plugin

--- bin/fbsdbot.rb
+++ bin/fbsdbot.rb
@@ -22,25 +22,12 @@
 	IRCEvent.add_callback('privmsg') do |event| 
 
 	     # only handle pubmsgs here ( channel equals my nick if this is a PRIVMSG )
-		 unless event.channel == bot.nick
-		 	#bot.send_message( event.from, "event: #{event.inspect}") 
-		 	#bot.send_message( event.from, "bot: #{bot.inspect}")
-		 	if event.message == "!die"
-		 	  puts "Reconnecting!"
-				bot.send_quit()
-			elsif event.message =~ /^!(.+)/
+			if event.message =~ /^!(.+)/
 			  line = $1.split
-			  if $commands[line.first].nil?
-			    bot.send_message(event.channel, "What?!?")
-			  else
+			  unless $commands[line.first].nil?
 			    $commands[line.shift][1].call(event, line.join(' '))
 			  end
 			end
-		 else
-			## PRIVMSG
-			## XXX: not working: 
-			# FBSDBot.handle_privmsg(event)
-     end 
 	end
 
   bot.connect
