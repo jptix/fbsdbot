bea3052559a00f72cf9208d885670705adc5b61a
Daniel Bond <gitbits@danielbond.org>
2007-07-04 06:10:27 +0200

    - plugin for audacious/np

---
+++ plugins/now_playing.rb
@@ -0,0 +1,14 @@
+class Now_playing < PluginBase
+     
+   def cmd_np(event, line)
+			at = "/usr/bin/audtool"
+			song = `#{at} --current-song`	
+			bitrate =  `#{at} --current-song-bitrate-kbps`
+			answer = "Mr_Bond is playing: #{song} [#{bitrate}Kb/s]"
+			unless event.from == "Mr_Bond"
+				answer = "#{event.nick}'s music is to gay to mention here!"
+			end
+			reply(event,answer)
+	 end
+   
+end
