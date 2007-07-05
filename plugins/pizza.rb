0085572990b4373874db02e7730de3a7bc6faa3e
Daniel Bond <gitbits@danielbond.org>
2007-07-05 19:54:47 +0200

    added "word" timings

--- plugins/pizza.rb
+++ plugins/pizza.rb
@@ -3,10 +3,16 @@ FBSDBot::Plugin.define "PizzaHighlight" do
 	author "Daniel Bond"
 	version "0.0.2"
 
-	@time = Time.new
 
 	def on_msg_pizza(a)
-	 a.reply("pizza confirmed at #{@time}") 
+	 a.reply("pizza confirmed at #{Time.now}") 
+	end
+
+	def on_msg(a)
+		if a.message.match(/(.+?) now/)
+			m = $1.sub(/^!/)
+			a.reply("#{$1} confirmed at #{Time.now}")
+		end
 	end
 
 end 
