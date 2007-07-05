97982dc5a5ff0ac72f0699dbf460f8699b01a1f8
Daniel Bond <gitbits@danielbond.org>
2007-07-05 12:29:21 +0200

    change in auth-plugin

--- plugins/auth.rb
+++ plugins/auth.rb
@@ -2,7 +2,13 @@ FBSDBot::Plugin.define "AuthHandler" do
 	author "Daniel Bond"
 	version "0.0.2"
 
-	def on_privmsg_auth(event,line)
-		$bot.send_message(event.from, "ok")
+	def privreply(e,msg)
+		$bot.send_message(e.from, msg)
+	end
+
+	def on_privmsg_auth(e,line)
+		return privreply(e, "You are logged in") if( $auth.is_authenticated?(e) )
+
+		privreply(e, "syntax: <handle> <pass>") unless line.match(/^(\w+)\s(\w+)/)
 	end
 end 
