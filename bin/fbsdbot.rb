4733943092c08cc6eae6f0bbd18ba7de4e65e24a
Daniel Bond <gitbits@danielbond.org>
2007-07-05 15:46:31 +0200

    - fixed last merge - added pizza plugin

--- bin/fbsdbot.rb
+++ bin/fbsdbot.rb
@@ -6,56 +6,6 @@ $: << $plugins_active
 
 module FBSDBot
 
-	 class Action
-
-#<IRCEvent:0xb77a58ac
-# @channel="#bot-test.no",
-# @event_type="privmsg",
-# @from="Mr_Bond",
-# @hostmask="d@niel.no",
-# @message="!auth",
-# @stats=["Mr_Bond", "d@niel.no", "PRIVMSG", "#bot-test.no"],
-# @user=#<IRCUser:0xb77a571c @username="Mr_Bond">>
-
-
-			attr_reader :nick, :channel, :message, :hostmask
-			
-			def initialize(bot,event)
-				@bot = bot
-				@nick = nil
-				@channel = nil
-				@message = nil
-				@hostmask = nil
-					
-				case event.event_type.to_sym
-					when :privmsg
-						
-						@nick = event.from
-						@message = event.message.gsub(/!\S+/,'').strip unless(event.message.nil?)
-						@hostmask = event.hostmask
-
-						# private / public?
-						if event.channel == bot.nick
-							@type = :privmsg
-							@respond_to = @nick
-						else
-							@type = :pubmsg
-							@channel = event.channel
-							@respond_to = @channel
-					  end	
-				end
-			end
-
-			def reply(msg)
-				if @type == :privmsg
-					@bot.send_message(@respond_to, msg)
-				elsif @type == :pubmsg
-					@bot.send_message(@respond_to, "#{@nick}, #{msg}")
-				end
-			end
-			
-	 end
-
    class Bot
       attr_accessor :commands, :hooks, :config, :irc, :auth
       attr_reader :threads, :command_count, :start_time
