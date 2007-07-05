c752b9ee845c3c1792f3fee1c46e45f84110f8f4
jp_tix <kreatix@gmail.com>
2007-07-05 15:07:11 +0200

    web2.rb now has a class inside :)
    On branch jp_experimental
    
    	modified:   lib/hooks.rb
    	modified:   plugins/web2.rb

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -27,26 +27,26 @@ module FBSDBot
   end
 
   def self.define(name, &block)
-		@name = name
+      @name = name
     p = new
     p.instance_eval(&block)
     Plugin.registered_plugins[name.to_sym] = p
   end
-
+  
   extend PluginSugar
   def_field :author, :version 
 	end
 
-### this under plugins/
-	Plugin.define "foo" do
-	  author "Daniel Bond"
-  	version "0.0.1"
+## this under plugins/
+  Plugin.define "foo" do
+    author "Daniel Bond"
+    version "0.0.1"
   
-  	# stuff
-  	def on_pubmsg_commands(event,line)  # becomes a singleton method
-			#puts bot.inspect
-			$bot.irc.send_message(event.channel, "#{event.from}, I cannot tell you my commands yet, sorry! :(")
-  	end
-	end
+    # stuff
+    def on_pubmsg_commands(event,line)  # becomes a singleton method
+      #puts bot.inspect
+      $bot.irc.send_message(event.channel, "#{event.from}, I cannot tell you my commands yet, sorry! :(")
+    end
+  end
 
 end
--- plugins/web2.rb
+++ plugins/web2.rb
@@ -1,34 +1,46 @@
 require 'net/http'
 require 'cgi'
 
+
 FBSDBot::Plugin.define("web") {
    author "jp_tix"
    version "0.0.1"
-   
+
+   class Web
+      def google(line)
+         if !line or line.empty?
+            return 'USAGE: google <search string>'
+         end
+
+         Net::HTTP.start('www.google.com') do |http|
+            re = http.get("/search?ie=utf8&oe=utf8&q=#{CGI.escape(line.strip)}", { 'User-Agent' => 'FBSDBot' })
+            if re.code == '200'
+               if re.body =~ /<a href="([^"]+)" class=l>(.+?)<\/a>/
+                  link = $1
+                  desc = $2.gsub('<b>', "\x02").gsub('</b>', "\x0f")
+                  return CGI.unescapeHTML("#{link} (#{desc})")
+               elsif re.body =~ /did not match any documents/
+                  return 'Nothing found.'
+               else
+                  return "Error parsing Google output."
+               end
+            else
+               return "Google returned an error: #{re.code} #{re.message}"
+            end
+         end
+
+      end
+   end
+
+   @web = Web.new
+
+   # ==================
+   # = Plugin Methods =
+   # ==================
+
    def on_pubmsg_google(action) # becomes a singleton method
       line = action.message
-     
-      if !line or line.empty?
-         action.reply 'USAGE: google <search string>'
-         return
-      end
-
-      Net::HTTP.start('www.google.com') do |http|
-         re = http.get("/search?ie=utf8&oe=utf8&q=#{CGI.escape(line.strip)}", { 'User-Agent' => 'FBSDBot' })
-         if re.code == '200'
-            if re.body =~ /<a href="([^"]+)" class=l>(.+?)<\/a>/
-               link = $1
-               desc = $2.gsub('<b>', "\x02").gsub('</b>', "\x0f")
-               action.reply CGI.unescapeHTML("#{link} (#{desc})")
-            elsif re.body =~ /did not match any documents/
-               action.reply 'Nothing found.'
-            else
-               action.reply "Error parsing Google output."
-            end
-         else
-            action.reply "Google returned an error: #{re.code} #{re.message}"
-         end
-      end
+      action.reply @web.google(line)
 
    end
 }
