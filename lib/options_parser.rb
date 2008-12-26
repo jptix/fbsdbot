d54e67388d7cc2376f92300ed48e2182de1cab48
jptix <kreatix@gmail.com>
2008-12-26 05:10:44 +0100

    Use symbols in config YAML

--- lib/options_parser.rb
+++ lib/options_parser.rb
@@ -9,15 +9,15 @@ op = OptionParser.new do |o|
   o.banner = "Usage: #{$0} [options] [config file]"
   
   o.on "-n", "--nick=nick", "A nick to use" do |nick|
-    $config['nick'] = nick
+    $config[:nick] = nick
   end
 
   o.on "-h", "--host=host", "An IRC server." do |host|
-    $config['host'] = host
+    $config[:host] = host
   end
   
   o.on "-p", "--port=port", Integer, "A port." do |port|
-    $config['port'] = port
+    $config[:port] = port
   end
   
   o.on "--help", "You're looking at it." do
@@ -29,8 +29,9 @@ end
 op.parse!(ARGV)
 
 unless ARGV.empty?
+  debugger
   config_file = ARGV.first
-  $config.merge(YAML.load_file(config_file))
+  $config.merge!(YAML.load_file(config_file))
   puts "Loaded config."
 end
 
