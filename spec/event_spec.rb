b02735ec37eb37aae62784f466ab61e5d47fbc9b
jptix <kreatix@gmail.com>
2008-12-27 16:59:13 +0100

    Add event_spec, move the inspect specs there.

---
+++ spec/event_spec.rb
@@ -0,0 +1,15 @@
+require "#{File.dirname(__FILE__)}/spec_helper"
+
+describe "Event" do
+
+  it "should not show the @worker instance variable on #inspect" do
+    event = Event.new(mock('worker'))
+    event.inspect.should_not include("worker")
+  end
+  
+  it "should show other instance variables for #inspect" do
+    event = EndOfMotdEvent.new(mock('worker'), :server => 'irc.homelien.no') 
+    event.inspect.should =~ /^#<FBSDBot::EndOfMotdEvent\(:end_of_motd\):0x[0-9a-f]{6} @server=\"irc.homelien.no\">$/
+  end
+
+end
