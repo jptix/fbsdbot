e55a8078d41968effdf0868be7fb62349eebf6cd
Daniel Bond <gitbits@danielbond.org>
2008-12-27 11:19:48 +0100

    Renamed bin/fbsdbot-em.rb to bin/fbsdbot.rb, and removed more legacy code

--- bin/fbsdbot.rb
+++ bin/fbsdbot.rb
@@ -1,102 +1,31 @@
 #!/usr/bin/env ruby
 
- 
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/..')
 require 'lib/boot'
-require "lib/irc/connection"
+require 'lib/options_parser'
+require 'lib/irc/event_machine'
 
-require "lib/options_parser" # creates $config
-
+Log.level = :info
 
 module FBSDBot
-
   VERSION = "0.1"
-
-  class Bot
-    attr_accessor :commands, :hooks, :nick, :auth, :command_count
-    attr_reader :threads, :start_time
-
-    DIR = File.expand_path(File.dirname(__FILE__) + "/..")
-
-    def initialize(config)
-      @commands = []
-      @command_count = 0
-
-      @config = config
-      @nick = @config[:nick]
-      @host = @config[:host]
-      @port = @config[:port]
-      @ircname = @config[:ircname].nil? ? "FBSDBot running on Ruby #{RUBY_VERSION}" : @config['ircname']
-
-      @start_time = Time.now
-      Log.info "starting bot..."
-      
-      @auth = FBSDBot::Authentication.new
-      load_plugins
-    end
-
-    def run
-      $stdout.sync = true
-      @irc = IRC::Connection.new(@nick, @host, :port => @port, :real_name => @ircname )
-      @irc.add_callback(:nickname_in_use) {|event| @irc.change_nick( FBSDBot::Helpers::NickObfusicator.run(@irc.nick) ) }
-      
-      @irc.add_callback(:end_of_motd) do |event|
-        @config[:channels].each do |ch|
-          @irc.join_channel(ch)
-          Log.info "joined channel: #{ch}"
-        end
-      end
-
-      @irc.add_callback(:private_message) do |event|
-        if event.message =~ /^!(\S+)/ or event.nick == @irc.nick
-          command = event.message.sub(/^!/, '').split[0]
-          return if command.nil?
-          FBSDBot::Plugin.find_plugins("on_msg_#{command}".to_sym, event)
-        else
-          FBSDBot::Plugin.find_plugins(:on_msg, event )
-        end
-      end
-
-      @irc.add_callback(:ctcp_action) do |event|
-        FBSDBot::Plugin.find_plugins(:on_ctcp_action, event)
-      end
-      
-      @irc.add_callback(:join) {|event| FBSDBot::Plugin.find_plugins(:on_join, event) }
-      @irc.add_callback(:part) {|event| FBSDBot::Plugin.find_plugins(:on_part, event) }
-      @irc.add_callback(:quit) {|event| FBSDBot::Plugin.find_plugins(:on_quit, event) }
-      
-      @irc.add_callback(:ctcp_version) do |event|
-        event.reply "FBSDBot v#{VERSION} - running on #{RUBY_VERSION}"
-      end
-
-      @irc.connect
-      @irc.join
-    end
-
-    private
-    
-    def load_plugins
-      require 'lib/corecommands'
-      Dir[DIR / 'plugins-active/*.rb'].each { |file| require file } if File.exists?(DIR / 'plugins-active')
-      puts "Loaded plugins: "
-      FBSDBot::Plugin.list_plugins
-      FBSDBot::Plugin.registered_plugins.each do |ident,p|
-        p.commands ? @commands += p.commands : nil
-      end
-      @commands.flatten!
-    end
-
-  end # class FBSDBot::Bot
-end # module FBSDBot
-
-begin
-  $bot = FBSDBot::Bot.new($config)
-  $bot.run
-ensure
-  puts "\nShutting down."
-  FBSDBot::Plugin.registered_plugins.each do |ident,p|
-    if p.respond_to?(:on_shutdown)
-      p.send(:on_shutdown)
-    end
-  end
 end
+
+EventMachine::run {  
+  require 'lib/corecommands'
+  Log.info "Loaded plugins:"
+  FBSDBot::Plugin.list_plugins
+
+  ### TODO: load the other plugins..
+  
+  FBSDBot::IRC::EMCore.connect(
+    :host     => $config[:host],
+    :nick     => $config[:nick],
+    :realname => $config[:realname] || "FBSDBot",
+    :username => $config[:username] || "fbsd",
+    :channels => $config[:channels]
+  )
+  # todo, define errback for handling reconnecting
+
+}
+
