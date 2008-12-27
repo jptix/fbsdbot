d21f85e883f432243b7b7ce56d6d74ecbed2a4ea
jptix <kreatix@gmail.com>
2008-12-27 17:21:44 +0100

    Add start of plugin_spec

---
+++ spec/plugin_spec.rb
@@ -0,0 +1,30 @@
+require "#{File.dirname(__FILE__)}/spec_helper"
+
+describe "Plugin" do
+  after(:each) do
+    Plugin.reset!
+  end
+
+  describe "#define" do
+    it "should add the defined plugin to it's list of plugins" do
+      Plugin.define('foobar') { }
+
+      plugin = Plugin.registered_plugins[:foobar]
+
+      plugin.should be_instance_of(Plugin)
+      plugin.name.should == 'foobar' 
+    end
+  end
+
+  describe "#reset" do
+    it "should remove all registered plugins / event handlers" do
+      plugin = Plugin.define("foo") { def on_cmd_foo(event); end }
+      
+      Plugin.registered_plugins.should_not be_empty
+      Plugin.reset!
+      Plugin.registered_plugins.should be_empty
+    end
+  end
+
+  
+end
