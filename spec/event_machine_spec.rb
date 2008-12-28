23ef7b793a506ab8a67b77a9f9e1c1d4db8e4adc
Daniel Bond <gitbits@danielbond.org>
2008-12-28 03:38:04 +0100

    Commiting faulty rspec, so rSpecMeister jptix can save the day

--- spec/event_machine_spec.rb
+++ spec/event_machine_spec.rb
@@ -1,24 +1,35 @@
 require "#{File.dirname(__FILE__)}/spec_helper"
 require 'lib/irc/event_machine'
 
+include IRC::Constants
+
 describe "IRC::EMCore" do
 
   before(:each) do
     @core = IRC::EMCore.new(nil)
   end
   
-  it "should buffer lines until getting EOL" do
-    @core.receive_data("").should == 0
-    @core.receive_data("\r").should == 0
-    @core.receive_data("\r\n").should == 0
-    @core.receive_data("\r\nhei").should == 0
-    @core.receive_data("b\r\n").should == 1
-    @core.receive_data("foo\r\n\r\n\r\n\r\nbar\r\n").should == 2
+  describe "'receive_data'" do
     
-    @core.should_receive(:produce_event).twice.with("foo\r\n")
-    @core.receive_data("\r\nfoo\r\nfoo\r\n\r\n")
+    it "should not buffer empty lines" do
+      @core.receive_data("").should == 0    
+    end
+    
+    it "should not buffer lines with only EOL" do
+      @core.receive_data(EOL).should == 0
+      @core.receive_data("\r\n").should == 0
+    end
+    
+    it "should send lines to event_producer with terminating EOL" do
+      @core.receive_data("")
+      @core.receive_data(EOL)
+      @core.receive_data("bar\r\n")
+      
+      m = mock('event_producer')
+      @core.instance_variable_set("@event_producer", m)
+      m.should_receive(:parse_line).with("bar\r\n")
+
+    end
   end
-  
-  
   
 end
