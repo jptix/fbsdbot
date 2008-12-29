6732b688e102bc24c2ff34f326acb3884dcd8193
jptix <kreatix@gmail.com>
2008-12-29 02:22:50 +0100

    ci test: fix build

--- spec/irc_parser_spec.rb
+++ spec/irc_parser_spec.rb
@@ -21,7 +21,7 @@ describe "FBSDBot::IRC::Parser" do
   
   it "should parse notices" do
     val = parse_message("NOTICE AUTH :*** Looking up your hostname...\r\n")
-    val[:command].should == 'NTICE'
+    val[:command].should == 'NOTICE'
   end
   
   it "should parse a host name prefix" do
