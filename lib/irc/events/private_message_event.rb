28f910731d84b255487e8910caeee8512627ab59
Daniel Bond <gitbits@danielbond.org>
2008-12-31 06:43:40 +0100

    Added @nick to PrivateMessageEvent

--- lib/irc/events/private_message_event.rb
+++ lib/irc/events/private_message_event.rb
@@ -2,9 +2,12 @@ module FBSDBot
   class PrivateMessageEvent < Event
     include Replyable
     
+    attr_reader :nick
+    
     def initialize(connection, opts = {})
       super(connection)
       setup(opts)
+      @nick = opts[:nick]
     end
     
   end
