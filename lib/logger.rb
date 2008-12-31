c09846821007637a2c4b01694783231e1931053a
jptix <kreatix@gmail.com>
2008-12-31 05:03:49 +0100

    New options: -c/--color, -d/--debug
    Can't use $config in boot.rb since the specs never loads a config.

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,18 +1,20 @@
 module FBSDBot
   class Logger
     attr_reader :level
-
+    attr_accessor :color
+    
     LOG_LEVELS = {
       :debug => 0,
       :info  => 1,
       :warn  => 2,
       :error => 3,
       :fatal => 4,
-      :off   => nil
+      :off   => 5
     }
 
     def initialize
       @level = :debug
+      @color = false
     end
     
     def level=(level)
@@ -22,7 +24,7 @@ module FBSDBot
       
       @level = level
     end
-
+    
     def debug(msg, obj = nil)
       log :debug, msg, obj
     end
@@ -53,9 +55,16 @@ module FBSDBot
       
       return unless this_level >= current_level
       
-      out = this_level >= LOG_LEVELS[:warn] ? $stderr : $stdout
+      out, red = this_level >= LOG_LEVELS[:warn] ? [$stderr,true] : [$stdout,false]
       msg = msg.inspect unless msg.respond_to?(:to_str)
-      out.puts "#{Time.now.strftime("%F %T")} (#{type}) #{obj} :: #{msg}"
+      
+      if @color
+        out.puts "\e[90m#{Time.now.strftime("%F %T")}\e[0m (\e[#{red ? '31' : '33'}m#{type}\e[0m) #{obj} :: \e[1m#{msg}\e[0m"
+      else
+        out.puts "#{Time.now.strftime("%F %T")} (#{type}) #{obj} :: #{msg}"
+      end
     end
+    
+    
   end
 end
