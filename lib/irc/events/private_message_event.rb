c8621fa515ef9273b0bdb377e9a25acb5c8331e9
jptix <kreatix@gmail.com>
2008-12-31 16:55:41 +0100

    Removed PrivateMessageEvent#nick, using event.user.nick in plugins instead.

--- lib/irc/events/private_message_event.rb
+++ lib/irc/events/private_message_event.rb
@@ -2,12 +2,9 @@ module FBSDBot
   class PrivateMessageEvent < Event
     include Replyable
     
-    attr_reader :nick
-    
     def initialize(connection, opts = {})
       super(connection)
       setup(opts)
-      @nick = opts[:nick]
     end
     
   end
