3daa0d5c3dbb013c37267046a390649a97ce45c7
jptix <kreatix@gmail.com>
2009-01-09 20:21:00 +0100

    Forgot file.

---
+++ lib/irc/events/kick_event.rb
@@ -0,0 +1,13 @@
+module FBSDBot
+  class KickEvent < Event
+
+    attr_reader :user, :message, :channel, :nick
+    
+    def initialize(conn, opts = {})
+      super(conn)
+      @channel, @nick, @message = opts[:params]
+      @user    = fetch_user(*opts.values_at(:nick, :user, :host))
+    end
+    
+  end
+end
